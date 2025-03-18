cd2404603367d5326b7f279f688a0c79289240d0
Augustvilliam <willyp@hotmail.se>
2025-03-18 17:15:36 +0100

    Massa mög

---
+++ ASPAssignment/Controllers/NavigationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPAssignment.Controllers;
+
+public class NavigationController : Controller
+{
+    public IActionResult LoadProjects()
+    {
+        return PartialView("_ProjectView");
+    }
+    public IActionResult LoadTeamMembers()
+    {
+        return PartialView("_TeamMembers");
+    }
+}
---
+++ ASPAssignment/wwwroot/js/navigation.js
@@ -0,0 +1,19 @@
+﻿
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("load-projects").addEventListener("click", function () {
+        loadPartialView("/Navigation/LoadProjects");
+    });
+
+    document.getElementById("load-team-members").addEventListener("click", function () {
+        loadPartialView("/Navigation/LoadTeamMembers");
+    });
+
+    function loadPartialView(url) {
+        fetch(url)
+            .then(response => response.text())
+            .then(html => {
+                document.getElementById("dynamic-content").innerHTML = html;
+            })
+            .catch(error => console.error("Error loading view:", error));
+    }
+});
