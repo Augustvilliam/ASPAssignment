6084528f5547302f88eebbb46dc3add47cf3e28a
Villiamremote <willyp@hotmail.se>
2025-04-24 13:06:53 +0200

    more bugfixes

--- ASPAssignment/wwwroot/js/transitions.js
+++ ASPAssignment/wwwroot/js/transitions.js
@@ -5,13 +5,4 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 10);
 });
 
-    document.addEventListener('DOMContentLoaded', function() {
-  const dyn = document.getElementById('main-hero');
-  setTimeout(() => dyn.classList.add('fade-in'), 10);
 
-  // observera förändringar i barn-noder
-  new MutationObserver(() => {
-        dyn.classList.remove('fade-in');
-    setTimeout(() => dyn.classList.add('fade-in'), 10);
-  }).observe(dyn, {childList: true });
-});
